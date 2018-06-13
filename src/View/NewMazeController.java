18bb103a98d5a33da1e3c0a291aab63955cfb45e
ilan <ilanpogreb01@gmail.com>
2018-06-13 18:39:39 +0300

    new maze function done. need to remove row, column, generate buttons from main scene

--- src/View/NewMazeController.java
+++ src/View/NewMazeController.java
@@ -1,21 +1,29 @@
 package View;
 
 import ViewModel.ViewModel;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class NewMazeController {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewMazeController implements Initializable {
     public TextField txt_columns;
     public TextField txt_rows;
     public Button btn_play;
     public Button btn_cancel;
     public ChoiceBox choice_character;
-    public ImageView image_character;
 
     private MazeDisplayer mazeDisplayer;
     private ViewController viewController;
@@ -23,18 +31,22 @@ public class NewMazeController {
     private Stage stage;
 
 
-
     public void setViewModel(ViewModel viewModel) {
         this.viewModel = viewModel;
 //        bindProperties(viewModel);
     }
 
-    public void setStage(Stage stage){ this.stage = stage;}
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
-    public void setMazeDisplayer(MazeDisplayer mazeDisplayer){ this.mazeDisplayer = mazeDisplayer; }
+    public void setMazeDisplayer(MazeDisplayer mazeDisplayer) {
+        this.mazeDisplayer = mazeDisplayer;
+    }
 
-    public void setViewController(ViewController viewController){ this.viewController = viewController; }
-
+    public void setViewController(ViewController viewController) {
+        this.viewController = viewController;
+    }
 
 
     public void generateMaze() {
@@ -56,7 +68,16 @@ public class NewMazeController {
         }
     }
 
+
+
     public void cancel(ActionEvent actionEvent) {
         stage.close();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choice_character.getItems().removeAll(choice_character.getItems());
+        choice_character.getItems().addAll("Pickle Rick", "Coming Soon");
+        choice_character.getSelectionModel().select("Pickle Rick");
     }
 }
